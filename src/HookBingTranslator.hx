25d273621aeb39445d4e70900273066afeacdccb
r32 <56782055@qq.com>
2023-05-02 00:37:19 +0800

    no old value

--- src/HookBingTranslator.hx
+++ src/HookBingTranslator.hx
@@ -18,14 +18,14 @@ class HookBingTranslator {
 		return len >= 2 && s.fastCodeAt(len - 1) == ".".code && s.fastCodeAt(len - 2) == ".".code;
 	}
 
-	static function rolling( old : String, lvl : Int ) {
+	static function rolling( lvl : Int ) {
 		tid = -1;
 		var cur = fromId(TOUT).value;
 		var len = cur.length;
 		if (lvl < 0) {
 			cur = chrome.I18n.getUILanguage() == "zh-CN" ? "查词失败" : "query failed";
-		} else if (cur == old || inQuerying(cur, len) || cur == " ") {
-			tid = window.setTimeout(rolling, 300, old, lvl - 1);
+		} else if (inQuerying(cur, len) || cur == " ") {
+			tid = window.setTimeout(rolling, 300, lvl - 1);
 			return;
 		}
 		LOG('(rolling)runtime.sendMessage({value : $cur, kind : respone})');
@@ -34,13 +34,12 @@ class HookBingTranslator {
 
 	@:keep public static function run( ens : String ) {
 		if (ens != null) {
-			var old = fromId(TOUT).value;
 			var input = fromId(TIN);
 			input.value = ens;
 			input.click();
 			if (tid > 0)
 				window.clearTimeout(tid);
-			tid = window.setTimeout(rolling, 300, old, 20); // 6 seconds
+			tid = window.setTimeout(rolling, 300, 20); // 6 seconds
 		}
 		voice();
 	}
